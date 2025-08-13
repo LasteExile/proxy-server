06ec0e5f3aaf6f7f279c31eb4a947c7aabf03507
Yehor Bryzhatyi <bryzhatyi.yehor@pinesoftware.com.cy>
2025-08-13 19:18:56 +0300

    Added separate client and server websoket handlers

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import logging
+import asyncio
 from typing import Literal, Dict, Any
 
 import requests
 from requests.exceptions import RequestException
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket as ServerWebSocket
 from fastapi import Query
 from fastapi.responses import Response, JSONResponse
 from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
-from websockets.asyncio.client import connect as ws_connect
+from websockets.asyncio.client import connect as ClientWebSocket
 
 
 logger = logging.getLogger(__name__)
@@ -52,19 +53,68 @@ def send(
     return Response(str(response.text))
 
 
+async def websocket_client_to_server(
+    *,
+    server_websocket: ServerWebSocket,
+    client_websocket: ClientWebSocket,
+) -> None:
+    try:
+        while True:
+            msg = await server_websocket.receive_text()           
+            await client_websocket.send(msg)                        
+    except Exception:
+        pass
+
+
+async def websocket_server_to_client(
+    *,
+    server_websocket: ServerWebSocket,
+    client_websocket: ClientWebSocket,
+) -> None:
+    try:
+        while True:
+            msg = await client_websocket.recv()                     
+            await server_websocket.send_text(msg)                  
+    except Exception:
+        pass
+
+
 @app.websocket("/ws")
 async def ws(
-    websocket: WebSocket,
+    server_websocket: ServerWebSocket,
     url: str = Query(required=True),
 ):
+    await server_websocket.accept()
+
+    async with ClientWebSocket(url) as client_websocket:
+        task1 = asyncio.create_task(websocket_client_to_server(
+            server_websocket=server_websocket,
+            client_websocket=client_websocket,
+        ))
+        task2 = asyncio.create_task(websocket_server_to_client(
+            server_websocket=server_websocket,
+            client_websocket=client_websocket,
+        ))
+        done, pending = await asyncio.wait(
+            {task1, task2},
+            return_when=asyncio.FIRST_COMPLETED
+        )
+
+        for t in pending:
+            t.cancel()
+
+        await asyncio.gather(*pending, return_exceptions=True)
+        try:
+            await server_websocket.close()
+        except Exception:
+            pass
+
+@app.websocket("/ws-echo")
+async def ws_echo(
+    websocket: ServerWebSocket,
+):
     await websocket.accept()
-
-    async with ws_connect(url) as client_websocket:
-        while True:
-            data = await websocket.receive_text()
-            await client_websocket.send(data)
-            message = await client_websocket.recv()
-            await websocket.send_text(message)
-
-
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(data)
 
